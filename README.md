f80199c3afc5bf928c2862b8f7ccc556350877b8
Georges Sleimen <95876833+Georgiooooo@users.noreply.github.com>
2022-09-27 13:33:23 +0200

    variable2.py

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 this project is to predict the winner in a match of tennis
 
 ## variables
-variable 1:  the history average ace all time for each player
+variable 1:  Ace average for each player all time
---
+++ variable 2.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('atp_final.csv')
+
+
+# History table of each player By Surface
+def surface_player(player, surface, table):
+    df_player = df[(table['first_player'] == player) & (table['surface'] == surface) |
+                   (table['second_player'] == player) & (table['surface'] == surface)]
+
+    return df_player
+
+
+player1_AceAverage_surface = [0]
+
+# This script is for calculate the Ace average history by surface for the first player
+for i in range(1, len(df)):
+
+    player1 = df.loc[i]['first_player']
+    player1_surface = df.loc[i]['surface']
+    df_player1 = surface_player(player1, player1_surface, df[0:i - 1])
+    df_player1 = df_player1.reset_index(drop=True)
+
+    ace_number = 0  # number of aces
+    if len(df_player1) > 0:
+        for j in range(len(df_player1)):
+            if df_player1.loc[j]['first_player'] == player1:
+                ace_number = ace_number + df_player1.loc[j]['first_player_ace']
+
+            elif df_player1.loc[j]['second_player'] == player1:
+                ace_number = ace_number + df_player1.loc[j]['second_player_ace']
+
+        player1_AceAverage_surface.append(round(ace_number / len(df_player1), 1))
+
+    else:
+        player1_AceAverage_surface.append(np.nan)
+
+player2_AceAverage_surface = [0]
+# This script is for calculate the Ace average history by surface for the second player
+for i in range(1, len(df)):
+    player2 = df.loc[i]['second_player']
+    player2_surface = df.loc[i]['surface']
+
+    df_player2 = surface_player(player2, player2_surface, df[0:i - 1])
+    df_player2 = df_player2.reset_index(drop=True)
+
+    ace_number = 0
+    if len(df_player2) > 0:
+        for j in range(len(df_player2)):
+            if df_player2.loc[j]['first_player'] == player2:
+                ace_number = ace_number + df_player2.loc[j]['first_player_ace']
+
+            elif df_player2.loc[j]['second_player'] == player2:
+                ace_number = ace_number + df_player2.loc[j]['second_player_ace']
+
+        player2_AceAverage_surface.append(round(ace_number / len(df_player2), 1))
+
+    else:
+        player2_AceAverage_surface.append(np.nan)
