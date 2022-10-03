c941555c707eb361eb6805a89ff7126c4454512a
Georges Sleimen <95876833+Georgiooooo@users.noreply.github.com>
2022-10-03 12:46:50 +0200

    variable 3

--- variable_3.py
+++ variable_3.py
@@ -13,7 +13,7 @@ def player_history_by_surface(player, surface, table):
 
 
 # This part is for calculate the average winning history by surface for the first player
-player1_win_prob_surface_alltime = [0]
+player1_win_avr_surface_alltime = [0]
 for i in range(1, len(df)):
     player1 = df.loc[i]['first_player']
     player1_surface = df.loc[i]['surface']
@@ -21,19 +21,41 @@ for i in range(1, len(df)):
     df_player1 = player_history_by_surface(player1, player1_surface, df[0:i - 1])
     df_player1 = df_player1.reset_index(drop=True)
 
-    l = 0
-    m = 0
+    winning_cumulation = 0
     if len(df_player1) > 0:
-        for i in range(len(df_player1)):
-            if df_player1.loc[i]['first_player'] == player1 and df_player1.loc[i]['winner_player'] == 1:
-                l = l + 1
-            elif df_player1.loc[i]['second_player'] == player1 and df_player1.loc[i]['winner_player'] == 2:
-                l = l + 1
-            elif df_player1.loc[i]['first_player'] == player1 and df_player1.loc[i]['winner_player'] == 2:
-                m = m + 1
-            elif df_player1.loc[i]['second_player'] == player1 and df_player1.loc[i]['winner_player'] == 1:
-                m = m + 1
-        player1_win_prob_surface_alltime.append(round(l / len(df_player1), 3))
+        for j in range(len(df_player1)):
+            if df_player1.loc[j]['first_player'] == player1 and df_player1.loc[j]['winner_player'] == 1:
+                winning_cumulation = winning_cumulation + 1
+            elif df_player1.loc[j]['second_player'] == player1 and df_player1.loc[j]['winner_player'] == 2:
+                winning_cumulation = winning_cumulation + 1
+        player1_win_avr_surface_alltime.append(round(winning_cumulation / len(df_player1), 3))
 
     else:
-        player1_win_prob_surface_alltime.append(np.nan)
+        player1_win_avr_surface_alltime.append(np.nan)
+
+df['player1_win_avr_surface_alltime'] = player1_win_avr_surface_alltime
+
+# This part is for calculate the average winning history by surface for the second player
+player2_win_avr_surface_alltime = [0]
+for i in range(1, len(df)):
+    player2 = df.loc[i]['second_player']
+    player2_surface = df.loc[i]['surface']
+
+    df_player2 = player_history_by_surface(player2, player2_surface, df[0:i - 1])
+    df_player2 = df_player2.reset_index(drop=True)
+
+    winning_cumulation = 0
+    if len(df_player2) > 0:
+        for j in range(len(df_player2)):
+            if df_player2.loc[j]['first_player'] == player2 and df_player2.loc[j]['winner_player'] == 1:
+                winning_cumulation = winning_cumulation + 1
+            elif df_player2.loc[j]['second_player'] == player2 and df_player2.loc[j]['winner_player'] == 2:
+                winning_cumulation = winning_cumulation + 1
+        player2_win_avr_surface_alltime.append(round(winning_cumulation / len(df_player2), 3))
+
+    else:
+        player2_win_avr_surface_alltime.append(np.nan)
+
+df['player2_win_avr_surface_alltime'] = player2_win_avr_surface_alltime
+
+df.to_csv('atp_final.csv', index=False)
